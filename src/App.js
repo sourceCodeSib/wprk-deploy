2b3448983cfd1a62b19d1f788f560bdc61d1afa2
sourceCodeSib <sibi13055@gmail.com>
2022-08-15 01:09:54 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Content">
+        <h1>Sibi Simon</h1>
+        <h4>Coming soon..</h4>
+      </div>
+    </div>
+  );
+}
+
+export default App;
